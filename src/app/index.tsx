b3d05033f1c324edab05c6c9139876d87dd2abdb
Sadan009 <imamsadan009@gmail.com>
2025-12-03 19:14:42 +0530

    Configure Expo Router

---
+++ src/app/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello There from Expo Router</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
